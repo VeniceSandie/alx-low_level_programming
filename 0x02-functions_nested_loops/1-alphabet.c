11686ee8e31c4dcc52fa964e0186942ddbf68705
VeniceSandie <sandisiwe2405@gmail.com>
2022-09-13 02:18:01 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+* main - entry point
+*
+* Description: write a function that prints the alphabet, in lowercase, followed by a new line
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+{
+	putchar(n;)
+}
+	_putchar('\n');
+}
