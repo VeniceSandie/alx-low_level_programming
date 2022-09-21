186c4615e80426726766e17765ce615c0eaee862
VeniceSandie <sandisiwe2405@gmail.com>
2022-09-21 09:50:25 -0700

    main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
 *_strcat - concatenates the string pointed to by @src to
 *the end of the string pointed to by @dest
 *@dest: String that will be appended
-@src: String to be concatenated upon
+*@src: String to be concatenated upon
 *
 Return: returns pointer to @dest
 */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,11 +12,11 @@ int_strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-char *strng_touperr(char *);
+char *string_touper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif/*MAIN_H*/
+#endif/*MAIN_H*i/
