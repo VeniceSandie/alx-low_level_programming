aa0cdb778f2e451c6435fc171875a00437420e62
VeniceSandie <sandisiwe2405@gmail.com>
2022-10-06 06:48:45 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,7 +7,7 @@
 * @min: The first value of the array.
 * @max: The last value of the array.
 *
-* Return: If min > max or the function fails - NULL.
+* Return: If min > max or the function fails - NULL
 *         Otherwise - a pointer to the newly created array.
 */
 int *array_range(int min, int max)
