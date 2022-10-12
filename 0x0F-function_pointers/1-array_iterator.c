b2b22d25b8778d299e687897c28808c7c02e667d
VeniceSandie <sandisiwe2405@gmail.com>
2022-10-12 10:53:52 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,10 +15,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array && size > 0 && action)
-		return;
-
+	{
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
+	}
 }
